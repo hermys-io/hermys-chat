e3c438466a9ecea19306080dd72afe7fb4a88bb8
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2024-04-29 00:18:36 -0300

    chore: resolve pipe error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { ChatBody } from "@/components/chat/chat-body";
 import { ChatFooter } from "@/components/chat/chat-footer";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="w-full h-screen flex flex-col bg-background">
       <ChatHeader />
       <ChatBody />
-      <ChatFooter />
+      <Suspense>
+        <ChatFooter />
+      </Suspense>
     </main>
   );
 }
