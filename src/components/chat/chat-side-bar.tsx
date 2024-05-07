6799a7080ac38dd1e66a74c6569231da8e5a3b22
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2024-05-07 14:28:12 -0300

    feat: add selected chat bg

--- src/components/chat/chat-side-bar.tsx
+++ src/components/chat/chat-side-bar.tsx
@@ -75,9 +75,9 @@ export default function ChatSideBar(props: ChatSideBarProps) {
         </Button>
       </header>
 
-      <div className="flex flex-col gap-6 w-[300px] h-full pt-4 px-4 bg-secondary">
+      <div className="flex flex-col gap-6 w-[300px] h-full pt-4 bg-secondary border-r-2">
         {/* Header */}
-        <div className="flex justify-between items-center text-secondary-foreground">
+        <div className="flex justify-between items-center px-4 text-secondary-foreground">
           <div className="text-2xl text-bold font-bold">Chats</div>
           <div className="flex items-center">
             <Button
@@ -91,9 +91,9 @@ export default function ChatSideBar(props: ChatSideBarProps) {
         </div>
 
         {/* Search */}
-        <div className="relative">
+        <div className="relative px-4">
           <SearchIcon
-            className="absolute top-[50%] left-[12px]  translate-y-[-50%]"
+            className="absolute top-[50%] left-[28px] translate-y-[-50%]"
             size={20}
           />
           <Input
@@ -105,7 +105,7 @@ export default function ChatSideBar(props: ChatSideBarProps) {
         </div>
 
         {/* Active chats */}
-        <div className="flex flex-col gap-4 w-full">
+        <div className="flex flex-col w-full">
           {getFilteredData(editais).map((edital) => (
             <div
               key={edital.id}
@@ -170,13 +170,21 @@ interface ChatItemProps {
 const ChatItem = (props: ChatItemProps) => {
   const { data } = props;
 
+  const [currentSelectedChat, setCurrentSelectedChat] = useAtom(selectedChat);
+
   const getStatus = (status: string) => {
     if (status === "in_progress") return "Em andamento";
     return "";
   };
 
+  const isSelected = currentSelectedChat === data.id;
+
   return (
-    <div className="flex gap-3 w-full cursor-pointer">
+    <div
+      className={`flex gap-3 w-full cursor-pointer p-4 hover:bg-background hover:transition transition ${
+        isSelected ? "bg-background" : ""
+      }`}
+    >
       <div className="flex items-center justify-center min-w-12 min-h-12 rounded-full bg-primary">
         <Avatar>
           {/* TODO: Pegar alt da resposta da API */}
@@ -187,9 +195,10 @@ const ChatItem = (props: ChatItemProps) => {
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </div>
+
       <div className="flex flex-col grow gap-[2px] border-b-2 overflow-hidden">
         <span className="truncate">{data.name}</span>
-        <span className="text-sm text-muted-foreground truncate">
+        <span className="text-sm text-muted-foreground truncate ">
           {getStatus(data.status)}
         </span>
       </div>
